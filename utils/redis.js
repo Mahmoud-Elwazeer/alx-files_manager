e8aad430f1fb9dd591c3bd1e5354f246b5c1688a
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-12 03:35:45 +0200

    handle redis client

---
+++ utils/redis.js
@@ -0,0 +1,60 @@
+const redis = require('redis')
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('connect', () => {
+      // console.log('Redis Client Connected')
+    });
+    this.client.on('error', err => {
+      console.log(`Redis Client Error ${err}`)
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  get(key) {
+    return new Promise((res, rej) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(reply);
+        }
+      })
+    });
+  }
+
+  set(key, value, expireInSec) {
+    return new Promise((res, rej) => {
+      this.client.set(key, value, (err, reply) => {
+        if (err) {
+          rej(err);
+        } else {
+          if (expireInSec) {
+            this.client.expire(key, expireInSec);
+          }
+          res(reply);
+        }
+      });
+    });
+  }
+
+  del(key) {
+    return new Promise((res, rej) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(reply);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
+// export default redisClient;
