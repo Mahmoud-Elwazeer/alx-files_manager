0eaee37ed1a6c40834bf8ef4b44dad73247c8b86
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-13 02:46:36 +0200

    #

--- utils/file.js
+++ utils/file.js
@@ -2,24 +2,20 @@ import dbClient from './db';
 
 const ObjectId = require('mongodb').ObjectID;
 
-async function createFile(query) {
-  const createfile = await dbClient.db.collection('files').insertOne(query);
-  return createfile;
-}
-
-async function getFile(query) {
-  const file = await dbClient.db.collection('files').findOne(query);
-  return file;
-}
-
-async function getFilesById(id) {
-  const objectId = new ObjectId(id);
-  const file = await dbClient.db.collection('files').findOne({ _id: objectId });
-  return file;
-}
-
-module.exports = {
-  createFile,
-  getFile,
-  getFilesById,
+const fileUtils = {
+  async createFile(query) {
+    const createfile = await dbClient.db.collection('files').insertOne(query);
+    return createfile;
+  },
+  async getFile(query) {
+    const file = await dbClient.db.collection('files').findOne(query);
+    return file;
+  },
+  async getFilesById(id) {
+    const objectId = new ObjectId(id);
+    const file = await dbClient.db.collection('files').findOne({ _id: objectId });
+    return file;
+  },
 };
+
+module.exports = fileUtils;
--- utils/user.js
+++ utils/user.js
@@ -3,41 +3,33 @@ import dbClient from './db';
 
 const ObjectId = require('mongodb').ObjectID;
 
-async function getUserAndKey(req) {
-  const obj = { userId: null, key: null };
+const userUtils = {
+  async getUserAndKey(req) {
+    const obj = { userId: null, key: null };
 
-  const xToken = req.header('X-Token');
+    const xToken = req.header('X-Token');
 
-  if (!xToken) return obj;
+    if (!xToken) return obj;
 
-  obj.key = `auth_${xToken}`;
+    obj.key = `auth_${xToken}`;
 
-  obj.userId = await redisClient.get(obj.key);
+    obj.userId = await redisClient.get(obj.key);
 
-  return obj;
-}
-
-async function createUser(query) {
-  const createuser = await dbClient.db.collection('users').insertOne(query);
-  return createuser;
-}
-
-async function getUser(query) {
-  const user = await dbClient.db.collection('users').findOne(query);
-  return user;
-}
-
-async function getUserById(userId) {
-  const objectId = new ObjectId(userId);
-  const userObj = await dbClient.db.collection('users').findOne({ _id: objectId });
-  return userObj;
-}
-
-// module.exports = userUtils;
-// export default userUtils;
-module.exports = {
-  getUserAndKey,
-  createUser,
-  getUser,
-  getUserById,
+    return obj;
+  },
+  async createUser(query) {
+    const createuser = await dbClient.db.collection('users').insertOne(query);
+    return createuser;
+  },
+  async getUser(query) {
+    const user = await dbClient.db.collection('users').findOne(query);
+    return user;
+  },
+  async getUserById(userId) {
+    const objectId = new ObjectId(userId);
+    const userObj = await dbClient.db.collection('users').findOne({ _id: objectId });
+    return userObj;
+  },
 };
+module.exports = userUtils;
+// export default userUtils;
